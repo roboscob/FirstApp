ea741359ac7d5b64d4e1e603431c3d387bb4e690
carlosgarcia <garciac4@nku.edu>
2022-01-28 14:14:42 -0500

    Initial Commit

---
+++ Shared/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  Shared
+//
+//  Created by Carlos Garcia on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
